294c1422e893dcb71224b701f44fdde45c369e33
iheb saad <ihebsaad@gmail.com>
2026-01-12 12:45:10 +0100

    update immo2

--- resources/views/clients/documents/templates/mandat_immo_2.blade.php
+++ resources/views/clients/documents/templates/mandat_immo_2.blade.php
@@ -14,57 +14,52 @@
                 <div class="row">
                     <div class="col-md-4 mb-3">
                         <label>Civilité Client 1</label>
-                        <select name="civilite_client1" class="form-control" required>
-                            <option value="Monsieur" {{ ($data['civilite_client1'] ?? '') === 'Monsieur' ? 'selected' : '' }}>Monsieur</option>
-                            <option value="Madame" {{ ($data['civilite_client1'] ?? '') === 'Madame' ? 'selected' : '' }}>Madame</option>
-                        </select>
+                        <input type="text" name="civilite_client1" class="form-control" value="{{ $data['civilite_client1'] ?? ($client->civilite === 'M' ? 'Monsieur' : 'Madame') }}" readonly>
                     </div>
                     <div class="col-md-4 mb-3">
                         <label>Nom Client 1</label>
-                        <input type="text" name="nom_client1" class="form-control" value="{{ $data['nom_client1'] ?? $client->nom }}" required>
+                        <input type="text" name="nom_client1" class="form-control" value="{{ $data['nom_client1'] ?? $client->nom }}" readonly>
                     </div>
                     <div class="col-md-4 mb-3">
                         <label>Prénom Client 1</label>
-                        <input type="text" name="prenom_client1" class="form-control" value="{{ $data['prenom_client1'] ?? $client->prenom }}">
+                        <input type="text" name="prenom_client1" class="form-control" value="{{ $data['prenom_client1'] ?? $client->prenom }}" readonly>
                     </div>
                     <div class="col-md-6 mb-3">
                         <label>Adresse Client 1</label>
-                        <input type="text" name="adresse_client1" class="form-control" value="{{ $data['adresse_client1'] ?? $client->adresse_complete }}">
+                        <input type="text" name="adresse_client1" class="form-control" value="{{ $data['adresse_client1'] ?? $client->adresse_complete }}" readonly>
                     </div>
                     <div class="col-md-6 mb-3">
                         <label>Date de naissance Client 1</label>
-                        <input type="date" name="date_naissance_client1" class="form-control" value="{{ $data['date_naissance_client1'] ?? ($client->date_naissance ? $client->date_naissance->format('Y-m-d') : '') }}">
+                        <input type="text" name="date_naissance_client1" class="form-control" value="{{ $data['date_naissance_client1'] ?? ($client->date_naissance ? $client->date_naissance->format('d/m/Y') : '') }}" readonly>
                     </div>
                 </div>
 
+                @if($client->conjoint_nom)
                 <hr>
-                <h6>Client 2 (optionnel - si couple)</h6>
+                <h6>Conjoint</h6>
                 <div class="row">
                     <div class="col-md-4 mb-3">
-                        <label>Civilité Client 2</label>
-                        <select name="civilite_client2" class="form-control">
-                            <option value="">-- Non applicable --</option>
-                            <option value="Monsieur" {{ ($data['civilite_client2'] ?? '') === 'Monsieur' ? 'selected' : '' }}>Monsieur</option>
-                            <option value="Madame" {{ ($data['civilite_client2'] ?? '') === 'Madame' ? 'selected' : '' }}>Madame</option>
-                        </select>
+                        <label>Civilité Conjoint</label>
+                        <input type="text" name="civilite_client2" class="form-control" value="{{ $data['civilite_client2'] ?? ($client->conjoint_civilite === 'M' ? 'Monsieur' : 'Madame') }}" readonly>
                     </div>
                     <div class="col-md-4 mb-3">
-                        <label>Nom Client 2</label>
-                        <input type="text" name="nom_client2" class="form-control" value="{{ $data['nom_client2'] ?? '' }}">
+                        <label>Nom Conjoint</label>
+                        <input type="text" name="nom_client2" class="form-control" value="{{ $data['nom_client2'] ?? $client->conjoint_nom }}" readonly>
                     </div>
                     <div class="col-md-4 mb-3">
-                        <label>Prénom Client 2</label>
-                        <input type="text" name="prenom_client2" class="form-control" value="{{ $data['prenom_client2'] ?? '' }}">
+                        <label>Prénom Conjoint</label>
+                        <input type="text" name="prenom_client2" class="form-control" value="{{ $data['prenom_client2'] ?? $client->conjoint_prenom }}" readonly>
                     </div>
                     <div class="col-md-6 mb-3">
-                        <label>Adresse Client 2</label>
-                        <input type="text" name="adresse_client2" class="form-control" value="{{ $data['adresse_client2'] ?? '' }}">
+                        <label>Adresse Conjoint</label>
+                        <input type="text" name="adresse_client2" class="form-control" value="{{ $data['adresse_client2'] ?? $client->adresse_complete }}" readonly>
                     </div>
                     <div class="col-md-6 mb-3">
-                        <label>Date de naissance Client 2</label>
-                        <input type="date" name="date_naissance_client2" class="form-control" value="{{ $data['date_naissance_client2'] ?? '' }}">
+                        <label>Date de naissance Conjoint</label>
+                        <input type="text" name="date_naissance_client2" class="form-control" value="{{ $data['date_naissance_client2'] ?? ($client->conjoint_date_naissance ? \Carbon\Carbon::parse($client->conjoint_date_naissance)->format('d/m/Y') : '') }}" readonly>
                     </div>
                 </div>
+                @endif
             </div>
         </div>
 
@@ -74,11 +69,11 @@
                 <div class="row">
                     <div class="col-md-6 mb-3">
                         <label>Montant du crédit immobilier (€)</label>
-                        <input type="number" name="montant_credit" class="form-control" value="{{ $data['montant_credit'] ?? '' }}" step="0.01">
+                        <input type="number" name="montant_credit" class="form-control" value="{{ $data['montant_credit'] ?? '' }}" step="0.01" placeholder="Ex: 250000">
                     </div>
                     <div class="col-md-6 mb-3">
                         <label>Apport personnel (€)</label>
-                        <input type="number" name="apport_personnel" class="form-control" value="{{ $data['apport_personnel'] ?? '' }}" step="0.01">
+                        <input type="number" name="apport_personnel" class="form-control" value="{{ $data['apport_personnel'] ?? '' }}" step="0.01" placeholder="Ex: 50000">
                     </div>
                     <div class="col-md-6 mb-3">
                         <label>Objet du financement</label>
@@ -117,7 +112,7 @@
                         <input type="date" name="date_debut_mandat" class="form-control" value="{{ $data['date_debut_mandat'] ?? now()->format('Y-m-d') }}" required>
                     </div>
                     <div class="col-md-4 mb-3">
-                        <label>Date de fin du mandat</label>
+                        <label>Date de fin du mandat (3 mois)</label>
                         <input type="date" name="date_fin_mandat" class="form-control" value="{{ $data['date_fin_mandat'] ?? now()->addMonths(3)->format('Y-m-d') }}" required>
                     </div>
                 </div>
@@ -130,7 +125,7 @@
                 <div class="row">
                     <div class="col-md-12 mb-3">
                         <label>Montant des honoraires (€)</label>
-                        <input type="number" name="montant_honoraires" class="form-control" value="{{ $data['montant_honoraires'] ?? '' }}" step="0.01">
+                        <input type="number" name="montant_honoraires" class="form-control" value="{{ $data['montant_honoraires'] ?? '' }}" step="0.01" placeholder="Ex: 2000">
                     </div>
                 </div>
             </div>
