87a988aad0918d9c94eeaa2a5c7a6caca03be20c
iheb saad <ihebsaad@gmail.com>
2026-02-28 16:57:46 +0100

    immo1

--- resources/views/clients/documents/pdf/mandat_immo_1.blade.php
+++ resources/views/clients/documents/pdf/mandat_immo_1.blade.php
@@ -67,16 +67,17 @@
             www.parfipro.com – Tel : 06-34-68-07-95<br>
             Votre conseiller ParFiPro : {{ $data['nom_conseiller'] }}
         </p>
-    </div>
-
-    <div class="section">
-        <div class="section-title">Intermédiation en opérations de banque et service de paiement</div>
         <p>
             ParFiPro est immatriculée auprès de l'ORIAS sous le n°200 01 570 (www.orias.fr) au titre des activités 
             réglementées suivantes :<br>
             <strong>Intermédiation en opérations de banque et service de paiement</strong><br>
             Courtier en opérations de banque et service de paiement (IOB)
         </p>
+    </div>
+
+    <div class="section">
+        <div class="section-title">Intermédiation en opérations de banque et service de paiement</div>
+
         
         <p style="margin-top: 8px;"><strong>Partenaires bancaires et en services de paiement du mandant</strong></p>
         <div class="partner-list">
@@ -95,6 +96,29 @@
             * Quintet<br>
             * Bill
         </div>
+        <b>Rémunération :</b>
+        <p style="margin-top: 8px;">
+        1. Par les partenaires bancaires et financiers : un commissionnement au titre de la mission d’intermédiation calculé en
+        pourcentage du montant des crédits accordés et soumis à plafonnement.<br>
+        Chaque partenaire détermine le pourcentage et le plafond de cette commission.
+        </p>
+        <p style="margin-top: 8px;">
+        2. Honoraires de courtage :<br>
+        La rémunération (commission et honoraires de courtage) n’est due qu’après versement des fonds, dans le cadre des
+        missions d’intermédiation.<br>
+        Liste des établissements bancaires et en services de paiement qui représentent sur l’année passée, une part supérieure au
+        tiers du chiffre d’affaires, au titre de l’activité d’intermédiation : NEANT<br>
+        Liste des établissements bancaires et en services de paiement détenant, directement ou indirectement plus de 10% des
+        droits de vote ou du capital de notre cabinet : NEANT<br>
+        Liste des établissements bancaires et en services de paiement où notre cabinet a une participation, directe ou indirecte,
+        supérieure à 10 % de droits de vote ou de capital : NEANT<br>
+        Service de conseil indépendant portant sur des crédits immobiliers, hypothécaires, professionnels<br>
+        Le conseiller propose un service de conseil au sens de l’article L. 519-1-1 du Code monétaire et financier portant sur des
+        contrats de crédit immobiliers, hypothécaires, professionnels.<br>
+        Les recommandations émises par le conseiller à ce titre portent sur :<br>
+        • Sur une large gamme de contrats de crédit disponibles sur le marché ; lors de la fourniture de ce service, notre cabinet
+        perçoit habituellement des honoraires de conseil d’un montant libre fixé au début de la mission.
+        </p>
 
         <p style="margin-top: 8px; font-size: 8px;">
             Le conseiller peut percevoir des honoraires versés par le client en cas de fourniture d'un conseil indépendant.
@@ -104,23 +128,31 @@
     <div class="section">
         <div class="section-title">Intermédiation en assurance</div>
         <p>
-            Catégorie d'intermédiaire *ParFiPro est une société de courtage d'assurance positionnée dans la catégorie « b » selon l'article 
+            <b>Catégorie d'intermédiaire </b><br>
+            *ParFiPro est une société de courtage d'assurance positionnée dans la catégorie « b » selon l'article 
             L.520 II 1 du Code des Assurances, sous le contrôle de l'Autorité de Contrôle Prudentiel et de Résolution 
             ACPR, 61 rue Taitbout, 75346 Paris Cedex 9. Elle n'est pas soumise à une obligation contractuelle de 
-            travailler exclusivement avec une ou plusieurs entreprises d'assurance.
+            travailler exclusivement avec une ou plusieurs entreprises d'assurance.            
+        </p>
+        <p>
+            <b>Rémunération : </b><br>
+            La rémunération perçue pour l’activité de courtier en assurance provient d’une part de commissions ou rétrocessions
+            versées par les compagnies d’assurance ou/et honoraires d’étude qui vous sont directement facturés. 
         </p>
     </div>
 
     <div class="section">
-        <div class="section-title">Service de conseil indépendant portant sur des crédits immobiliers, hypothécaires, professionnels</div>
+        <div class="section-title">Conseil – Audit</div>
         <p>
-            Le conseiller propose un service de conseil au sens de l'article L. 519-1-1 du Code monétaire et financier portant sur des 
-            contrats de crédit immobiliers, hypothécaires, professionnels.
+            Le conseiller peut proposer au client les prestations de conseil suivantes :<br>
+            Une prestation de conseil de Niveau 2 : émettre une recommandation personnalisée consistant à
+            expliquer au client pourquoi, parmi plusieurs solutions, une ou plusieurs correspondent le mieux à
+            ses exigences et besoins.<br>
+            Dans ce cadre, le conseiller est non soumis à une obligation contractuelle de travailler exclusivement avec une ou plusieurs établissements de crédit ou entreprises d’assurance.
         </p>
-        <p>Les recommandations émises par le conseiller à ce titre portent sur :</p>
         <p>
-            • Sur une large gamme de contrats de crédit disponibles sur le marché ; lors de la fourniture de ce service, notre cabinet 
-            perçoit habituellement des honoraires de conseil d'un montant libre fixé au début de la mission.
+            <b>Rémunération : </b><br>
+            Les études de faisabilité peuvent être rémunérés par une somme forfaitaire allant de 500€ HT à 10 000€ HT en fonction de la durée, de la complexité et des sommes concernées. 
         </p>
     </div>
 
@@ -131,211 +163,66 @@
             CPI 7501 2025 000 000 230, sans détention de fonds, délivrée le 22/05/2025.
         </p>
         <p>
-            Le conseiller peut proposer au client les prestations de conseil suivantes :<br>
-            Une prestation de conseil de Niveau 2 : émettre une recommandation personnalisée consistant à 
-            expliquer au client pourquoi, parmi plusieurs solutions, une ou plusieurs correspondent le mieux à 
-            ses exigences et besoins.
-        </p>
-        <p>
-            Dans ce cadre, le conseiller est non soumis à une obligation contractuelle de travailler exclusivement 
-            avec une ou plusieurs établissements de crédit ou entreprises d'assurance.
-        </p>
-    </div>
-
-    <div class="section">
-        <div class="section-title">Rémunération</div>
-        <p>
-            La rémunération perçue pour l'activité de courtier en assurance provient d'une part de commissions ou rétrocessions 
-            versées par les compagnies d'assurance ou/et honoraires d'étude qui vous sont directement facturés.
-        </p>
-        <p>
-            <strong>1. Par les partenaires bancaires et financiers :</strong> un commissionnement au titre de la mission d'intermédiation calculé en 
-            pourcentage du montant des crédits accordés et soumis à plafonnement.<br>
-            Chaque partenaire détermine le pourcentage et le plafond de cette commission.
-        </p>
-        <p>
-            <strong>2. Honoraires de courtage :</strong><br>
-            La rémunération (commission et honoraires de courtage) n'est due qu'après versement des fonds, dans le cadre des 
-            missions d'intermédiation.
-        </p>
-        <p>
-            Liste des établissements bancaires et en services de paiement qui représentent sur l'année passée, une part supérieure au 
-            tiers du chiffre d'affaires, au titre de l'activité d'intermédiation : NEANT
-        </p>
-        <p>
-            Liste des établissements bancaires et en services de paiement détenant, directement ou indirectement plus de 10% des 
-            droits de vote ou du capital de notre cabinet : NEANT
-        </p>
-        <p>
-            Liste des établissements bancaires et en services de paiement où notre cabinet a une participation, directe ou indirecte, 
-            supérieure à 10 % de droits de vote ou de capital : NEANT
-        </p>
-    </div>
-
-    <div class="section">
-        <div class="section-title">Conseil – Audit</div>
-        <div class="box">
-            <p><strong>Rémunération :</strong></p>
-            <p>
-                Les études de faisabilité peuvent être rémunérés par une somme forfaitaire allant de 
-                500€ HT à 10 000€ HT en fonction de la durée, de la complexité et des sommes concernées.
-            </p>
-        </div>
-        <div class="box">
-            <p><strong>Rémunération :</strong></p>
-            <p>
-                Les activés de prestations immobilières seront rémunérées par des commissions à la charge du vendeur 
-                ou de l'acquéreur, en fonction du type de bien, et de la complexité du travail à effectuer.<br>
-                Cette rémunération sera comprise entre 1% HT et 6% HT (soit 1,20% TTC à 7,20% TTC).
-            </p>
-        </div>
-    </div>
-
-    <div class="section">
-        <div class="section-title">RCP</div>
-        <p>
-            Votre conseiller dispose, conformément à la loi et à tous les codes de bonne conduite de la CNCEF, d'une couverture en 
-            Responsabilité Civile Professionnelle et d'une Garantie Financière suffisantes couvrant ses diverses activités. Ces 
-            couvertures sont notamment conformes aux exigences du code monétaire et financier et du code des assurances. Elles 
+            Votre conseiller dispose, conformément à la loi et à tous les codes de bonne conduite de la CNCEF, d’une couverture en
+            Responsabilité Civile Professionnelle et d’une Garantie Financière suffisantes couvrant ses diverses activités. Ces
+            couvertures sont notamment conformes aux exigences du code monétaire et financier et du code des assurances. Elles
             sont souscrites auprès de AIG sous le numéro de polices : RD01452752A
         </p>
-        <table style="width: 100%; margin: 10px 0; font-size: 8px;">
-            <tr>
-                <td style="width: 33%; padding: 5px; border: 1px solid #000; text-align: center;">
-                    <strong>IOBSP IAS</strong><br>
-                    1.564.610 € par sinistre et 2.315.610 € par période d'assurance.
-                </td>
-                <td style="width: 33%; padding: 5px; border: 1px solid #000; text-align: center;">
-                    <strong>Garantie financière</strong><br>
-                    Pour des montants de 500.000€ par sinistre<br>
-                    Limité à 800.000€ par an
-                </td>
-                <td style="width: 34%; padding: 5px; border: 1px solid #000; text-align: center;">
-                    1.500.000€ par sinistre<br>
-                    Limité à 2.000.000€ par an
-                </td>
-            </tr>
+        <table  style="width: 100%; margin: 10px 0; font-size: 8px; border-collapse: collapse;">
+            <tr><th>Pour des montants de </th><th>IOBSP</th><th>IAS</th></tr>
+            <tr><td>RCP</td><td colspan="2">1.564.610 € par sinistre et 2.315.610 € par période d'assurance.</td></tr>
+            <tr><td>Garantie financière</td><td>500.000€ par sinistre Limité à 800.000€ par an</td><td>1.500.000€ par sinistre Limité à 2.000.000€ par an</td></tr>
         </table>
-        <p style="font-size: 8px;">
-            Votre conseiller s'est engagé à respecter intégralement tous les codes de bonne conduite de la CNCEF disponibles au siège 
-            de l'association ou sur www.cncef.org
+        <p>
+            Votre conseiller s’est engagé à respecter intégralement tous les codes de bonne conduite de la CNCEF disponibles au siège
+            de l’association ou sur www.cncef.org
+        </p>
+        <p>
+            <b>Rémunération : </b><br>
+            Les activés de prestations immobilières seront rémunérées par des commissions à la charge du vendeur ou de l’acquéreur, en fonction du type de bien, et de la complexité du travail à effectuer.<br>
+            Cette rémunération sera comprise entre 1% HT et 6% HT (soit 1,20% TTC à 7,20% TTC).
         </p>
     </div>
 
-    <div class="section">
+     <div class="section">
         <div class="section-title">Procédure de réclamation – médiation</div>
-        <p>
-            En cas de désaccord, une solution amiable sera envisagée en premier lieu après la réception 
-            d'une réclamation sur support durable adressée à :
-        </p>
-        <p>
-            Par courrier à l'adresse suivante :<br>
-            173 Boulevard Pereire – 75017 Paris
-        </p>
-        <p>
-            Par courriel à l'adresse suivante :<br>
-            rjacob.parfipro@gmail.com
-        </p>
-        <p>
-            Nous nous engageons à accuser réception de la réclamation dans un délai de dix jours ouvrables puis 
-            à y répondre dans un délai de deux mois à compter de la réception de la réclamation. En cas de litige 
-            et si la réponse apportée à sa réclamation ne lui apparaît pas satisfaisante, le client consommateur 
-            peut ensuite saisir le médiateur de la consommation suivant en vue de sa résolution amiable :
-        </p>
-        <p>
-            Par courrier à l'adresse suivante :<br>
-            CMAP – Service Médiation de la consommation<br>
-            39 avenue F.D. Roosevelt<br>
-            75008 Paris
-        </p>
-        <p>
-            Par courriel à l'adresse suivante :<br>
-            consommation@cmap.fr
-        </p>
-    </div>
+     </div>
 
     <div class="section">
         <div class="section-title">Traitement des données personnelles</div>
-        <p>
-            En application des dispositions de la loi n° 78-17 du 6 janvier 1978 et du Règlement 2016/679 du 
-            Parlement européen et du Conseil du 27 avril 2016 relatif à la protection des personnes 
-            physiques à l'égard du traitement des données à caractère personnel et à la libre circulation de 
-            ces données, le cabinet s'engage à ne collecter et traiter les données recueillies qu'au regard des 
-            finalités de traitement convenues entre le cabinet et son client, à préserver leur sécurité et 
-            intégrité, à ne communiquer ces informations qu'à des tiers auxquels il serait nécessaire de les 
-            transmettre en exécution des prestations convenues, et plus généralement à agir dans le cadre des 
-            exigences réglementaires auxquelles il est soumis. Le client est informé qu'il a le droit de demander 
-            au responsable de traitement l'accès aux données à caractère personnel, leurs catégories et leurs 
-            destinataires, la durée de leur conservation ou, à défaut, les critères utilisés pour déterminer 
-            cette durée, leur rectification, leur effacement et leur portabilité, ainsi que le droit de demander 
-            une limitation du traitement de ses données à caractère personnel, sur simple demande sur support 
-            durable (courrier, email, etc…).
-        </p>
-        <p>
-            L'identité et les coordonnées du responsable de traitement au sein du cabinet sont les suivantes :<br>
-            -Raphaël JACOB – 06-34-68-07-95 – rjacob.parfipro@gmail.com
-        </p>
-        <p>
-            L'identité et les coordonnées du délégué à la protection des données au sein du cabinet sont les suivantes :<br>
-            -Raphaël JACOB – 06-34-68-07-95 – rjacob.parfipro@gmail.com
-        </p>
-        <p>
-            Le client a le droit d'introduire une réclamation auprès de la Commission Nationale de l'Informatique 
-            et des Libertés (CNIL) à l'adresse suivante :<br>
-            Commission Nationale de l'Informatique et des Libertés (CNIL)<br>
-            3 Place de Fontenoy<br>
-            TSA 80715<br>
-            75334 PARIS CEDEX 07
-        </p>
-    </div>
+     </div>
 
     <div class="section">
         <div class="section-title">Autorité de tutelle</div>
-        <p>ACPR – Autorité de Contrôle Prudentiel et de Résolution - 61 rue Taitbout 75009 Paris</p>
-        
-        <div class="section-title" style="margin-top: 10px;">Registre</div>
-        <p>ORIAS – Organisme pour le registre unique des intermédiaires en assurance, banque et finance – 1 rue 
-            Jules Lefebvre 75009 Paris - www.orias.fr</p>
-        
-        <div class="section-title" style="margin-top: 10px;">Mise à jour</div>
-        <p>
-            Le Conseiller fait parvenir au client toute mise à jour de ces différentes informations, en lui communiquant par mail.<br>
-            Le client peut également obtenir à tout moment ces informations sur simple demande auprès du conseiller.
-        </p>
-    </div>
+     </div>
 
-    <div class="box" style="font-size: 8px; margin: 15px 0;">
-        <p>
-            Un crédit vous engage et doit être remboursé. Vérifiez vos capacités de remboursement avant de vous engager.<br>
-            Aux termes de l'article L. 519-6 du Code monétaire et financier, « il est interdit à toute personne physique ou morale 
-            qui apporte son concours, à quelque titre que ce soit et de quelque manière que ce soit, directement ou 
-            indirectement, à l'obtention ou à l'octroi d'un prêt d'argent, de percevoir une somme représentative de 
-            provision, de commissions, de frais de recherche, de démarches, de constitution de dossier ou d'entremise 
-            quelconque, avant le versement effectif des fonds prêtés. Il lui est également interdit, avant la remise 
-            des fonds et de la copie de l'acte, de présenter à l'acceptation de l'emprunteur des lettres de change, ou de lui faire 
-            souscrire des billets à ordre, en recouvrement des frais d'entremise ou des commissions mentionnés à l'alinéa précédent. 
-            Les infractions aux dispositions des premier et deuxième alinéa du présent article sont recherchées et constatées 
-            dans les conditions fixées à l'article L. 353-5 et sont punies des peines prévues à l'article L. 353-1 ». La diminution du 
-            montant des mensualités entraîne l'allongement de la durée de remboursement et majore le coût total du crédit. 
-            La réduction dépend de la durée restante des prêts rachetés.
-        </p>
+    <div class="section">
+        <div class="section-title">Registre</div>
+     </div>
+    <div class="section">
+        <div class="section-title">Mise à jour</div>
     </div>
-
     <div class="signature-area">
         <p>Fait à {{ $data['fait_a'] }}, le {{ \Carbon\Carbon::parse($data['date_document'])->format('d/m/Y') }}</p>
         
-        <table class="signatures">
+        <table class="signatures" style="width:50%">
             <tr>
                 <td>
                     <strong>[Client] -</strong><br>
                     Signature précédée de "Lu et approuvé"<br><br><br>
                 </td>
+            </tr>
+        </table>
+        <table class="signatures" style="width:50%">
+            <tr>
                 <td>
                     <strong>[Client] -</strong><br>
                     Signature précédée de "Lu et approuvé"<br><br><br>
                 </td>
+            </tr>
+            <tr>
                 <td>
-                    <strong>[Conseiller] - {{ $data['nom_conseiller'] }}</strong><br>
+                    <strong>[Client] -</strong><br>
                     Signature précédée de "Lu et approuvé"<br><br><br>
                 </td>
             </tr>
